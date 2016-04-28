0147b3024ac47d53f2c2c1227dee5e472a7e8b18
aifly <aifly_xc@163.com>
2016-04-28 18:05:52 +0800

    .

--- static/js/index.js
+++ static/js/index.js
@@ -20544,7 +20544,7 @@
 	                                    ),
 	                                    _react2['default'].createElement(
 	                                        _antd.Menu.Item,
-	                                        { key: '/../richimg/' },
+	                                        { key: '/ZPlatform/richimg/' },
 	                                        _react2['default'].createElement(_antd.Icon, { type: 'picture', style: { marginRight: '32px' } }),
 	                                        '富图片'
 	                                    )
