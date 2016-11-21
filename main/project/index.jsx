b32246ddeb000137fad33c3dfa4a43b570c9e501
bmyuan <bing-x.l.l@163.com>
2016-11-21 18:31:46 +0800

    get in 2016.11.21

--- main/project/index.jsx
+++ main/project/index.jsx
@@ -2,27 +2,19 @@ import React,{Component} from 'react';
 import {ZmitiValidateUser} from '../public/validate-user.jsx';
 import MainUI from '../components/Main.jsx';
 import './static/css/index.css';
-import Row from 'antd/lib/row';
-import 'antd/lib/row/style/css';
-import Col from 'antd/lib/col';
-import 'antd/lib/col/style/css';
-import Tabs from 'antd/lib/tabs';
-import 'antd/lib/tabs/style/css';
+import {Row,Col,Tabs,Table,Icon,Button,Checkbox,Modal,Form,Input,DatePicker,} from 'antd';
 const TabPane = Tabs.TabPane;
 import ZmitiSearchInput from '../components/zmiti-search-input.jsx';
-import Table from 'antd/lib/table';
-import 'antd/lib/table/style/css';
-import Icon from 'antd/lib/icon';
-import 'antd/lib/icon/style/css';
+const FormItem = Form.Item;
 
 class  ZmitiProject extends Component{
 	constructor(props) {
 		super(props);
 		this.state = {
-
+			visible:true
 		}
 	}
-	render() {
+render() {
 const columns = [{
   title: '编号',
   dataIndex: 'name',
@@ -77,15 +69,109 @@ const data = [{
 		var component =<div>
 			<Tabs tabPosition='left'>
           <TabPane tab="项目管理" key="1">
-			<Row>
-				<Col span={12} offset={6}><ZmitiSearchInput></ZmitiSearchInput></Col>
-			</Row>
-			<Table columns={columns} dataSource={data} />
+      		{/*项目管理内部框架*/}
+		  <Row>
+		  	<Col span={21} offset={0} className="pro-col-15 pro-row-15">
+		  		<h5 className="pro-name">中国文明网项目管理组</h5>
+		    </Col>
+		    <Col span={3} offset={0} className="pro-col-15 pro-row-15" >
+		  		<Button type="primary " onClick={()=>{this.setState({visible:true})}}>创建项目</Button>
+		    </Col>
+		  </Row>
+		  <Row>
+		  	<Col span={24} className="pro-col-15">
+		  		 <div className="pro-line"></div>
+		  	</Col>
+		  </Row>
+		  <Row>
+          	<Col span={24} offset={0} className="pro-col-15">
+	          	<Row>
+					<Col span={12} offset={0} className="pro-row-15"><ZmitiSearchInput></ZmitiSearchInput></Col>
+					<Col span={5}  className="pro-row-15 pro-col-15"><Checkbox defaultChecked="true" >只看正在进行的项目</Checkbox></Col>
+				</Row>
+				<Row>
+					<Col span={24} offset={0}>
+						<Table columns={columns} dataSource={data} />
+					</Col>
+				</Row>
+          	</Col> 
+          </Row>
+		<Modal title="新建项目" visible={this.state.visible}
+          onOk={this.createProject.bind(this)} onCancel={()=>{this.setState({visible:false})}}
+        >
+	         <Row>
+	         	<Col span={24} className="pro-row-15">
+	         		<Form horizontal>
+	         			<FormItem
+	         				label="项目名称"
+	         				labelCol={{span:5}}
+	         				wrapperCol={{span:12}}	         				
+	         				validateStatus="validating"
+	         				help=""
+	         				>
+	         				<Input placeholder="请录入项目名称" id="validating" />
+	         			</FormItem>
+	         			
+	         			<FormItem
+	         				label="项目负责人"
+	         				labelCol={{span:5}}
+	         				wrapperCol={{span:12}}	         				
+	         				validateStatus="validating"
+	         				help=""
+	         				>
+	         				<Input placeholder="请录入项目名称" id="validating" />
+	         			</FormItem>
+	         			<FormItem
+	         				label="项目成员"
+	         				labelCol={{span:5}}
+	         				wrapperCol={{span:12}}	         				
+	         				validateStatus="validating"
+	         				help=""
+	         				>
+	         				<Input placeholder="请录入项目名称" id="validating" />
+	         			</FormItem>
+	         			<FormItem
+	         				label="所属客户"
+	         				labelCol={{span:5}}
+	         				wrapperCol={{span:12}}	         				
+	         				validateStatus="validating"
+	         				help=""
+	         				>
+	         				<Input placeholder="请录入项目名称" id="validating" />
+	         			</FormItem>
+	         			<FormItem
+	         				label="起止时间"
+	         				labelCol={{span:5}}        				
+	         				help=""
+	         				>
+	         				<Col span="6">
+						        <FormItem validateStatus="" help="">
+						          <DatePicker />
+						        </FormItem>
+						    </Col>
+						    <Col span="1">
+						        <p className="ant-form-split">-</p>
+						    </Col>
+						    <Col span="6">
+						        <FormItem>
+						          <DatePicker />
+						        </FormItem>
+						    </Col>
+	         			</FormItem>
+	         		</Form>
+	         	</Col>
+	         </Row>
+        </Modal>
+		
           </TabPane>
           <TabPane tab="任务管理" key="2">Content of Tab 2</TabPane>
         </Tabs>
 		</div>;
-		return <MainUI component={component}></MainUI>
+		return <MainUI className="project" component={component}></MainUI>
+	}
+
+	createProject(){
+
 	}
 }
 
