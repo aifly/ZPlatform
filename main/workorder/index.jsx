1a6463f016cc670bfc3db6d9a0b04a30ac38c1f1
bmyuan <bing-x.l.l@163.com>
2017-03-29 18:49:33 +0800

    add workorder page

--- main/workorder/index.jsx
+++ main/workorder/index.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
-import './static/css/index.css';
+import './static/css/index.min.css';
 import ZmitiUserList  from '../components/zmiti-user-list.jsx';
 
-import { message,Select,Modal,Form , Input,Button, Row, Col,Switch,Radio,InputNumber,Popconfirm   } from '../commoncomponent/common.jsx';
+import { message,Select,Modal,Form , Input,Button, Row, Col,Switch,Radio,InputNumber,Popconfirm,DatePicker   } from '../commoncomponent/common.jsx';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+moment.locale('zh-cn');
 const RadioGroup = Radio.Group;
 let FormItem  = Form.Item;
 let Option = Select.Option;
@@ -36,9 +39,24 @@ class ZmitiWorkOrderApp extends Component {
             title:title,
             type:'workorder-1',
             rightType:"custom",
-            customRightComponent:<div>
-			
-			</div>
+            customRightComponent:<div className="zmiti-workorder-main-ui padding-10">
+				<Row className='zmiti-workorder-header'>
+					<Col  className='zmiti-workorder-header-inner' >我的工单</Col>
+				</Row>
+				<div className="zmiti-workorder-line"></div>
+				<Row gutter={10} type='flex' className='workorder-search '>
+					<Col span={2} className='zmiti-workorder-with100 workorder-heigth45'>工单编号：</Col>
+					<Col span={5} className={'workorder-heigth45'}><Input value={'工单编号录入'}/></Col>
+					<Col span={3} className={'zmiti-workorder-with60 workorder-heigth45 rig'}>时间:</Col>
+					<Col span={5} className={'workorder-heigth45 zmiti-workorder-with130 '}><DatePicker /></Col>
+					<Col span={3} className={'zmiti-workorder-with30 workorder-heigth45 cen'}>至:</Col>
+					<Col span={5} className={'workorder-heigth45 zmiti-workorder-with130 '}><DatePicker defaultValue={moment(new Date(), 'YYYY-MM-DD')}/></Col>
+					<Col span={3} className={'zmiti-workorder-with60 workorder-heigth45 rig'}>关键词:</Col>
+					<Col span={3} className={'workorder-heigth45'}><Input /></Col>
+					<Col span={5} className={'workorder-heigth45 lef'}><Button>查询</Button></Col>
+				</Row>
+
+		</div>
         }
   
 		var mainComponent = <div>
