c7b0ccfd6ab824459f3a3823248939e9b5f3be7d
bmyuan <bing-x.l.l@163.com>
2017-03-30 20:32:01 +0800

    workorder

--- main/workorder/index.jsx
+++ main/workorder/index.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './static/css/index.min.css';
 import ZmitiUserList  from '../components/zmiti-user-list.jsx';
 
-import { message,Select,Modal,Form , Input,Button, Row, Col,Switch,Radio,InputNumber,Popconfirm,DatePicker   } from '../commoncomponent/common.jsx';
+import { message,Select,Modal,Form , Input,Button, Row, Col,Switch,Radio,InputNumber,Popconfirm,DatePicker,Table   } from '../commoncomponent/common.jsx';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
 moment.locale('zh-cn');
@@ -26,10 +26,112 @@ class ZmitiWorkOrderApp extends Component {
 		this.state = {
 
             mainHeight:document.documentElement.clientHeight - 50,
+            dataSource:[],
+			workorderid:''
 
 		};
 	}
 	render() {
+
+        const columns = [{
+            title: '工单编号',
+            dataIndex: 'workorderid',
+            key: 'workorderid',
+			width:200,
+
+        }, {
+            title: '问题内容',
+            dataIndex: 'content',
+            key: 'content',
+        }, {
+            title: '工单类型',
+            dataIndex: 'workordertype',
+            key: 'workordertype',
+			width:100,
+            filters:[{
+                text:'财务类',
+                value:'0',
+            },{
+                text:'会员帐号类',
+                value:'1',
+            },{
+                text:'定制服务类',
+                value:'2',
+            }, {
+                text: '产品技术类',
+                value: '3',
+            },{
+                text:'其它类',
+                value:'4',
+            }
+            ],
+            onFilter:(value,record)=>value*1===record.workordertype,
+            sorter:(a,b)=>a.workordertype-b.workordertype,
+            render:(value,record)=>{
+                switch(value){
+                    case 0:
+                        return "财务类";
+                    case 1:
+                        return "会员帐号类";
+                    case 2:
+                        return "定制服务类";
+                    case 3:
+                        return "产品技术类";
+                    case 4:
+                        return "其它类";
+                }
+            }
+        }, {
+            title: '创建时间',
+            dataIndex: 'createtime',
+            key: 'createtime',
+			width:150,
+        }, {
+            title: '工单状态',
+            dataIndex: 'status',
+            key: 'status',
+			width:100,
+            filters:[{
+                text:'已受理',
+                value:'0',
+            },{
+                text:'已处理',
+                value:'1',
+            },{
+                text:'已确认',
+                value:'2',
+            },{
+                text:'已评价',
+                value:'3',
+            }
+            ],
+			onFilter:(value,record)=>value*1===record.status,
+			sorter:(a,b)=>a.status-b.status,
+			render:(value,record)=>{
+            	switch(value){
+					case 0:
+						return <div className='red'>已受理</div>;
+					case 1:
+						return <div className='green'>已处理</div>;
+					case 2:
+						return "已确认";
+					case 3:
+						return "已评价";
+				}
+			}
+
+        }, {
+            title: '操作',
+            dataIndex: 'operation',
+            key: 'operation',
+            width:100,
+			render:(text,recoder)=>(
+				<span><span className="workorder-gotodetail"> 查看</span><span className="workorder-del"> 删除</span>
+				</span>
+			)
+
+        }];
+
         var title = this.props.params.title || '服务支持中心';
         let props={
             userid:this.userid,
@@ -45,16 +147,17 @@ class ZmitiWorkOrderApp extends Component {
 				</Row>
 				<div className="zmiti-workorder-line"></div>
 				<Row gutter={10} type='flex' className='workorder-search '>
-					<Col span={2} className='zmiti-workorder-with100 workorder-heigth45'>工单编号：</Col>
-					<Col span={5} className={'workorder-heigth45'}><Input value={'工单编号录入'}/></Col>
-					<Col span={3} className={'zmiti-workorder-with60 workorder-heigth45 rig'}>时间:</Col>
-					<Col span={5} className={'workorder-heigth45 zmiti-workorder-with130 '}><DatePicker /></Col>
-					<Col span={3} className={'zmiti-workorder-with30 workorder-heigth45 cen'}>至:</Col>
-					<Col span={5} className={'workorder-heigth45 zmiti-workorder-with130 '}><DatePicker defaultValue={moment(new Date(), 'YYYY-MM-DD')}/></Col>
-					<Col span={3} className={'zmiti-workorder-with60 workorder-heigth45 rig'}>关键词:</Col>
-					<Col span={3} className={'workorder-heigth45'}><Input /></Col>
-					<Col span={5} className={'workorder-heigth45 lef'}><Button>查询</Button></Col>
+					<Col  className='zmiti-workorder-with100 workorder-heigth45'>工单编号：</Col>
+					<Col  className={'workorder-heigth45'}><Input value={this.state.workorderid} onChange={this.searchByWorkorderid.bind(this)}  placeholder="工单编号录入" /></Col>
+					<Col  className={'zmiti-workorder-with60 workorder-heigth45 rig'}>时间:</Col>
+					<Col  className={'workorder-heigth45 zmiti-workorder-with130 '}><DatePicker /></Col>
+					<Col  className={'zmiti-workorder-with30 workorder-heigth45 cen'}>至:</Col>
+					<Col  className={'workorder-heigth45 zmiti-workorder-with130 '}><DatePicker defaultValue={moment(new Date(), 'YYYY-MM-DD')}/></Col>
+					<Col  className={'zmiti-workorder-with60 workorder-heigth45 rig'}>关键词:</Col>
+					<Col  className={'workorder-heigth45'}><Input  defaultValue={''} placeholder="关键词"/></Col>
+					<Col  className={'workorder-heigth45 lef'}><Button onClick={this.search.bind(this)}>查询</Button></Col>
 				</Row>
+				<Table dataSource={this.state.dataSource} columns={columns} bordered/>
 
 		</div>
         }
@@ -67,13 +170,69 @@ class ZmitiWorkOrderApp extends Component {
 			);
 	}
 
+    search(){
+		var workorderid = this.state.workorderid;
+		if(workorderid === ''){
+			return;
+		}
+		var arr = [];
+		this.state.dataSource.forEach((item,i)=>{
+			if(item.workorderid.indexOf(workorderid)>-1){
+				arr.push(item);
+			}
+		});
+
+		this.setState({
+			dataSource:arr
+		})
+
+	}
+
+    searchByWorkorderid(e){
+        this.setState({
+            workorderid:e.target.value
+        },()=>{
+
+            this.dataSource = this.dataSource  || this.state.dataSource.concat([]) ;
+
+            this.state.dataSource = this.dataSource.filter((item)=>{
+                return  item.workorderid.indexOf(this.state.workorderid)>-1;
+            });
+            this.forceUpdate();
+        });
+    }
+
 	changeAccount(i){
 
 
 	}
 
 	componentDidMount() {
-		
+
+		var s = this;
+		$.ajax({
+			url:window.baseUrl+'user/get_workorder',
+			data:{
+				userid:s.userid,
+                getusersigid:s.getusersigid
+			},
+			success(data){
+				if(data.getret === 0){
+					s.state.dataSource = data.workorderinfo;
+					s.forceUpdate();
+				}
+                else if(data.getret === -3){
+                    message.error('您没有访问的权限,2秒后跳转到首页');
+                    setTimeout(()=>{
+                        location.href='/';
+                    },2000)
+                }
+                else{
+                    loginOut(data.getmsg,window.loginUrl,false);
+                }
+			}
+		})
+
 	}
 
 
@@ -82,13 +241,13 @@ class ZmitiWorkOrderApp extends Component {
 	componentWillMount() {
 
 
-		/*let {resizeMainHeight,validateUser,loginOut} = this.props;
+		let {resizeMainHeight,validateUser,loginOut} = this.props;
 
-		resizeMainHeight(this);	*/
+		resizeMainHeight(this);
 		
-		/*let {username,userid,getusersigid} = validateUser(()=>{},this);
+		let {username,userid,getusersigid} = validateUser(()=>{},this);
 		this.userid = userid;
-		this.getusersigid = getusersigid;*/
+		this.getusersigid = getusersigid;
 
 		
 	}
