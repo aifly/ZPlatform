f8d22ed0e07439b0c155ad1b599142547a92ef41
aifly <aifly_xc@163.com>
2017-04-28 18:54:24 +0800

    add peotry page

--- main/poetry/list.jsx
+++ main/poetry/list.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { message,Row,Col,Input,Button,Popconfirm,Tooltip } from '../commoncomponent/common.jsx';
+import { message,Input,Button,Modal,Tooltip,Popconfirm } from '../commoncomponent/common.jsx';
 
 import ZmitiUploadDialog from '../components/zmiti-upload-dialog.jsx';
 
@@ -21,25 +21,20 @@ class ZmitiPoetryListApp extends Component {
 		this.state = {
 			mainHeight:document.documentElement.clientHeight - 50,
 			isEntry:1,
+			currentState:0,
+			visible:true,
 			poetryList:[
 				  
 			],//聊天作品列表
+
+
 			data:{
 				shareTitle:'',//分享标题
 				shareDesc:'',//分享描述
 				shareImg:'',//分享图片300.jpg;
 				background:'',//聊天背景图片
-				bgSound:'',//背景音乐
-				username:'',
-				myHeadImg:'',
-				groupName:'',//群名称
-				title:'',
-				memberList:[
-					
-				],
-				talk:[
-					
-				]
+				bgSound:'',//背景音乐 
+				type:'SHI'
 			}
 		}; 
 	}
@@ -56,7 +51,7 @@ class ZmitiPoetryListApp extends Component {
 	}
 	render() {
 		var component = <div className='poetry-list-main-ui'>
-			<section className='poetry-list-C'>
+			{this.state.currentState === -1 && <section className='poetry-list-C'>
 				<ul className='poetry-list'>
 					<li  title='创建作品' onClick={this.createWork.bind(this)}>
 						<img src='./static/images/create.png'/>
@@ -79,11 +74,56 @@ class ZmitiPoetryListApp extends Component {
 						</li>
 					})}
 				</ul>
-			</section>					
+			</section>}
+			{this.state.currentState === 0 && <section className='poetry-create-C' style={{height:this.state.mainHeight}}>
+				<aside>
+					<img  draggable='false' className='poetry-iphone' src='./static/images/poetry-phone.png'/>
+				</aside>
+				<aside>
+					<div className='poetry-text' ref='poetry-text'>
+						<img ref='poetry-text-img'  draggable='false' src='./static/images/poetry-text.png'/>
+					</div>
+					<div onClick={this.showChooseType.bind(this)} className='poetry-creat-btn' style={{background:'url(./static/images/poetry-btn-bg.png) no-repeat center center / contain'}}>
+						创建
+					</div>
+					<div onClick={()=>{this.setState({currentState:-1})}} className='poetry-sea-list'>查看历史创建</div>
+				</aside>
+				 <Modal title="" visible={this.state.visible}
+				  width='800'
+		          onOk={this.handleOk} onCancel={this.handleCancel}
+		        >
+		          <div className='poetry-type-C'>
+			         <section className='poetry-type-list'>
+			         	<aside onClick={this.modifyType.bind(this,'SHI')}><img draggable='false' src={this.state.data.type==='SHI'?'./static/images/poetry-poetry1.png':'./static/images/poetry-poetry.png'}/></aside>
+			         	<aside  onClick={this.modifyType.bind(this,'CI')}><img draggable='false' src={this.state.data.type==='CI'?'./static/images/poetry-ci1.png':'./static/images/poetry-ci.png'}/></aside>
+			         	<aside onClick={this.modifyType.bind(this,'TONGYAO')}><img draggable='false' src={this.state.data.type==='TONGYAO'?'./static/images/poetry-tongyao1.png':'./static/images/poetry-tongyao.png'}/></aside>
+			         </section>
+			         <section className='poetry-line'></section>
+			         <section  className='poetry-type-list'>
+			         	<aside style={{opacity:0}}><img draggable='false' src='./static/images/poetry-poetry.png'/></aside>
+			         	<aside  onClick={this.modifyType.bind(this,'CUSTOM')}><img draggable='false' src={this.state.data.type==='TONGYAO'?'./static/images/poetry-custom1.png':'./static/images/poetry-custom.png'}/></aside>
+			         	<aside  style={{opacity:0}}><img draggable='false' src='./static/images/poetry-ci.png'/></aside>
+			         </section>
+			      </div>
+		        </Modal>
+			</section>}
 		</div>
 		return (
 			<MainUI component={component}></MainUI>
 		);
+	}
+
+	showChooseType(){
+		this.setState({
+			visible:true
+		})
+	}
+
+
+	modifyType(type){
+
+		this.state.data.type = type;
+		this.forceUpdate();
 	}
 
 	deletePoetry(worksid,i){
@@ -106,6 +146,109 @@ class ZmitiPoetryListApp extends Component {
 			}
 		})
 	}
+
+	flyParticleToImage(option) {
+          option = option || {}
+
+             var iWidth = option.container.offsetWidth,
+            	iHeight = option.container.offsetHeight;
+            option.container.innerHTML = "";
+           
+            var canvas = document.createElement("canvas");
+            canvas.style.transition = "1s opacity";
+            canvas.style.position = "absolute";
+            canvas.width = iWidth;
+            canvas.height = iHeight;
+            option.container.appendChild(canvas);
+            var stage = new createjs.Stage(canvas);
+            var container = new createjs.Container();
+
+            var outCanvas = createShape(canvas);
+            var outContext = outCanvas.getContext("2d");
+            var dots = getImageData(outCanvas, outContext);
+
+            var ball = [];
+
+            for (var i = 0; i < dots.length; i++) {
+                var shape = new createjs.Shape();
+                var x = Math.random() * canvas.width * 2,
+                    y = Math.random() * canvas.height * 2;
+                var circle = shape.graphics.beginFill('rgba('+dots[i].r+','+dots[i].g+','+dots[i].b+','+dots[i].a+')').drawRect(x, y, 2,2);
+                shape.posX = x;
+                shape.posY = y;
+                container.addChild(shape);
+                ball.push(shape);
+            }
+            container.x = 10;
+
+
+            stage.addChild(container);
+
+            stage.update();
+
+
+            function getImageData(outCanvas, outContext) {
+                var imgData = outContext.getImageData(0, 0, outCanvas.width, outCanvas.height);
+
+
+                var dots = [],
+                    x = 0,
+                    y = 0,
+                    gap = 4;
+
+                for (var x = 0; x < imgData.width; x += gap) {
+                    for (var y = 0; y < imgData.height; y += gap) {
+                        var i = (x + y * outCanvas.width) * 4;
+
+                        if (imgData.data[i + 3] > 128) {
+
+                            dots.push({
+                                x: x,
+                                y: y,
+                                r:imgData.data[i],
+                                g:imgData.data[i + 1],
+                                b:imgData.data[i + 2],
+                                a:imgData.data[i + 3]
+                            });
+                        }
+                    }
+                }
+                return dots;
+            }
+
+            function createShape() {
+                var outCanvas = document.createElement("canvas");
+
+                outCanvas.width = iWidth;
+                outCanvas.height = iHeight;
+                var context = outCanvas.getContext("2d");
+                var outStage = new createjs.Stage(outCanvas);
+
+                context.drawImage(option.img, 0, 0, iWidth, iHeight);
+
+                outCanvas.style.position = "absolute";
+                outCanvas.style.opacity = 0;
+                outCanvas.style.transition = "opacity 2s";
+
+                option.container.appendChild(outCanvas);
+
+                return outCanvas;
+            }
+
+
+            for (var i = 0; i < ball.length; i++) {
+                createjs.Tween.get(ball[i], { override: true }).wait(Math.random() * 800).to({ x: dots[i].x - ball[i].posX, y: dots[i].y - ball[i].posY, alpha: 1 }, 500, createjs.Ease.cubicInOut);
+            }
+            setTimeout(function () {
+                outCanvas.style.opacity = 1;
+                canvas.style.opacity = 0;
+                option.complate && option.complate();
+                createjs.Ticker.off("tick", stage);
+            }, 1500);
+            createjs.Ticker.setFPS(60);
+            createjs.Ticker.addEventListener("tick", stage);
+
+    }
 
 	save(worksid,viewpath){
 		var s = this;
@@ -210,7 +353,15 @@ class ZmitiPoetryListApp extends Component {
 				}
 			}
 		})
-		
+		setTimeout(()=>{
+			this.flyParticleToImage({
+				 container: this.refs['poetry-text'],
+	            img: this.refs['poetry-text-img'],
+	            complate: function () {
+
+	            }
+			});
+		},500)
 
 	}
  
