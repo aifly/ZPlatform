39e9c6a2d886e186de60ef3b68abc84b06930554
clearinfo <songxian@163.com>
2017-06-28 11:21:45 +0800

    song

--- main/training/index.jsx
+++ main/training/index.jsx
@@ -2,6 +2,7 @@ import './static/css/index.css';
 import React from 'react';
 import {message,Select,Modal,Form,Icon,Tag,Tooltip, Input,Button, Row, Col,Switch,Radio,InputNumber,DatePicker,Table ,moment,Spin} from '../commoncomponent/common.jsx';
 let Search = Input.Search;
+const FormItem = Form.Item;
 let Option = Select.Option;
 import $ from 'jquery';
 
@@ -21,33 +22,15 @@ import MainUI from '../components/Main.jsx';
             dataSource:[],
             alldataSource:[],
             countNum:0,
+            modpostDialogVisible:false,
+            realname:'',
+            gender:0,
+            mobile:'',
+            province:'',
+            rooms:'',
         }
+        this.currentId = -1;
     }
-
-    componentWillMount() {
-        let {resizeMainHeight,popNotice,validateUser,loginOut,validateUserRole,isSuperAdmin,isNormalAdmin,getUserDetail,listen,send} = this.props;
-        var {userid, getusersigid, companyid,username,isover,usertypesign}=validateUser(()=>{
-                loginOut('登录失效，请重新登录',window.loginUrl,false);
-            },this);
-            this.loginOut = loginOut;
-            this.listen = listen;
-            this.send = send;
-            this.popNotice = popNotice;
-            this.isSuperAdmin = isSuperAdmin;
-            this.isNormalAdmin = isNormalAdmin;
-            this.validateUserRole = validateUserRole;
-            this.getUserDetail = getUserDetail;
-            this.resizeMainHeight = resizeMainHeight;
-    }
-    componentDidMount(){
-        var s=this;
-       this.resizeMainHeight(this);
-       s.getuserlists();
-       s.loadData();
-
-    }
-
-
 
 
     render(){
@@ -58,6 +41,10 @@ import MainUI from '../components/Main.jsx';
             loginOut();
         },this);
         resizeMainHeight(this);
+        const formItemLayout = {
+           labelCol: {span: 6},
+           wrapperCol: {span: 14},
+        };
         const columns = [{
             title: '序号',
             dataIndex: 'key',
@@ -74,7 +61,7 @@ import MainUI from '../components/Main.jsx';
             title: '性别',
             dataIndex: 'gender',
             key: 'gender',
-            width:200,
+            width:80,
             render:function(text, record, index) {
                 if(text==0){
                     return "男"
@@ -93,13 +80,21 @@ import MainUI from '../components/Main.jsx';
             title: '省份',
             dataIndex: 'province',
             key: 'province',
-            width:200,
+            width:160,
 
         },{
             title: '房间号',
             dataIndex: 'rooms',
             key: 'rooms',
-            width:200,
+            width:160,
+        },{
+            title: '操作',
+            dataIndex: '',
+            key: '',
+            width:160,
+            render:(text,recoder,index)=>(
+                <span><Button onClick={this.editProduct.bind(this,recoder.id)}>编辑</Button> <Button onClick={this.delData.bind(this,recoder.id)}>删除</Button></span>              
+            )
         }]
 
        
@@ -109,7 +104,7 @@ import MainUI from '../components/Main.jsx';
                     <div className="zmiti-training-header">
                         <Row>
                             <Col span={8} className="zmiti-training-header-inner">文明网培训班</Col>
-                            <Col span={8} offset={8} className='zmiti-training-button-right'><Button type="primary" onClick={this.goback.bind(this)}><Icon type="left" />返回</Button></Col>
+                            <Col span={8} offset={8} className='zmiti-training-button-right'><Button type="primary" onClick={this.goback.bind(this)}><Icon type="left" />返回</Button><Button onClick={this.addProduct.bind(this)}>添加</Button></Col>
                         </Row>                      
                     </div>
                     <div className="zmiti-training-line"></div>
@@ -139,14 +134,93 @@ import MainUI from '../components/Main.jsx';
                     dataSource={this.state.dataSource} 
                     columns={columns} />
                 </div>
+                <Modal title="培训班" visible={this.state.modpostDialogVisible}
+                    onOk={this.trainform.bind(this)}
+                    onCancel={()=>{this.setState({modpostDialogVisible:false})}}
+                  >
+                    <Form>
+                        <FormItem
+                            {...formItemLayout}
+                            label="姓名"
+                            hasFeedback
+                            >
+                            <Input placeholder="姓名" 
+                            value={this.state.realname}
+                            onChange={(e)=>{this.state.realname=e.target.value;this.forceUpdate();}}
+                            />
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="性别"
+                            hasFeedback
+                            >
+                            <Input placeholder="性别" 
+                            value={this.state.gender}
+                            onChange={(e)=>{this.state.gender=e.target.value;this.forceUpdate();}}
+                            />
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="手机号"
+                            hasFeedback
+                            >
+                            <Input placeholder="手机号" 
+                            value={this.state.mobile}
+                            onChange={(e)=>{this.state.mobile=e.target.value;this.forceUpdate();}}
+                            />
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="省份"
+                            hasFeedback
+                            >
+                            <Input placeholder="省份" 
+                            value={this.state.province}
+                            onChange={(e)=>{this.state.province=e.target.value;this.forceUpdate();}}
+                            />
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="房间号"
+                            hasFeedback
+                            >
+                            <Input placeholder="房间号" 
+                            value={this.state.rooms}
+                            onChange={(e)=>{this.state.rooms=e.target.value;this.forceUpdate();}}
+                            />
+                        </FormItem>
+                    </Form>
+                </Modal>
             </div>
         </Spin>
         return(
             <MainUI component={component}></MainUI>
         )
     }
+    componentWillMount() {
+        let {resizeMainHeight,popNotice,validateUser,loginOut,validateUserRole,isSuperAdmin,isNormalAdmin,getUserDetail,listen,send} = this.props;
+        var {userid, getusersigid, companyid,username,isover,usertypesign}=validateUser(()=>{
+                loginOut('登录失效，请重新登录',window.loginUrl,false);
+            },this);
+            this.loginOut = loginOut;
+            this.listen = listen;
+            this.send = send;
+            this.popNotice = popNotice;
+            this.isSuperAdmin = isSuperAdmin;
+            this.isNormalAdmin = isNormalAdmin;
+            this.validateUserRole = validateUserRole;
+            this.getUserDetail = getUserDetail;
+            this.resizeMainHeight = resizeMainHeight;
+    }
+    componentDidMount(){
+        var s=this;
+       this.resizeMainHeight(this);
+       s.bindNewdata();
+       s.loadData();
+    }
+
     //获取数据列表
-    getuserlists(){
+    bindNewdata(){
         var s=this;
         $.ajax({
             //url:window.baseUrl + 'weixin/get_userscorelist/',
@@ -211,6 +285,64 @@ import MainUI from '../components/Main.jsx';
     //返回
     goback(){
         window.location='#/custom/'
+    }
+    //对话框新增加时
+    trainform(){
+        var s=this;
+        if(this.currentId!==-1){//编辑
+            s.setState({
+                modpostDialogVisible:false
+            });
+            s.bindNewdata();
+        }else{//添加
+            s.setState({
+                modpostDialogVisible:false
+            });
+            s.bindNewdata();
+        }
+    }
+    addProduct(){//添加
+        var s = this;
+        var userid = this.props.params.userid?this.props.params.userid:this.userid;
+        this.currentId=-1;
+        this.setState({
+            modpostDialogVisible:true,
+            realname:'',
+            gender:0,
+            mobile:'',
+            province:'',
+            rooms:'',
+        })
+        console.log(this.currentId,'currentId');
+    }
+    editProduct(id){//编辑
+        
+        var s = this;
+        this.currentId=id;
+        var userid = this.props.params.userid?this.props.params.userid:this.userid;
+        $.each(this.state.dataSource,function(index,item){          
+            if(item.id===id){
+                s.setState({
+                    modpostDialogVisible:true,
+                    realname:item.realname,
+                    gender:item.gender,
+                    mobile:item.mobile,
+                    province:item.province,
+                    rooms:item.rooms
+                })                              
+                //console.log(item.realname,s.state.realname);
+            }
+        })
+        console.log(this.currentId,'currentId');
+    }
+    delData(id){//删除
+        var userid = this.props.params.userid?this.props.params.userid:this.userid;
+        var params = {
+            userid:this.userid,
+            getusersigid:this.getusersigid,
+            id:id,
+        }
+        console.log(id,'params-del');
     }
   
 }
