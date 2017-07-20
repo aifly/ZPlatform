153f765d41151ed0e335e1857a780e30383a44e7
aifly <aifly_xc@163.com>
2017-07-20 18:13:16 +0800

    a

--- main/webpack.build.config.js
+++ main/webpack.build.config.js
@@ -4,7 +4,42 @@ var config = {
     entry: {
         'index': "./index.jsx",
         'admin':'./admin/index.jsx',
-        vendor: ['react','react-dom','iscroll','jquery']
+        react:['react','react-dom','./static/editor/froala_editor.min.js','./static/editor/align.min.js','./static/editor/colors.min.js'],
+        iscroll:['jquery','iscroll','./puzzle/static/js/createjs.js'],
+        echarts:['echarts/lib/echarts','echarts/lib/chart/map'],
+        antd:[
+        'antd/lib/button',
+        'antd/lib/message',
+        'antd/lib/input',
+        'antd/lib/table',
+        'antd/lib/modal',
+        'antd/lib/steps',
+        'antd/lib/tree',
+        'antd/lib/tag',
+        'antd/lib/slider',
+        'antd/lib/form',
+        'antd/lib/icon',
+        'antd/lib/radio',
+        'antd/lib/progress',
+        'antd/lib/tabs',
+        'antd/lib/switch',
+        'antd/lib/card',
+        'antd/lib/menu',
+        'antd/lib/date-picker',
+        'antd/lib/select',
+        'antd/lib/row',
+        'antd/lib/col',
+        'antd/lib/checkbox',
+        'antd/lib/popconfirm',
+        'antd/lib/badge',
+        'antd/lib/notification',
+        'antd/lib/spin',
+        'antd/lib/input-number',
+        'antd/lib/tooltip',
+        'antd/lib/layout',
+        'antd/lib/cascader',
+        'antd/lib/popover'
+        ]
     },
     output: {
         //publicPath: './static/js',
@@ -29,7 +64,7 @@ var config = {
          name:"vendor",  
       }),*/
         new webpack.optimize.CommonsChunkPlugin({
-           name:"vendor",  
+           name:['react','iscroll','echarts','antd'],  
         }),
         new webpack.optimize.UglifyJsPlugin({
           compress:{
