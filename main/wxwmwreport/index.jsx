641f6fb42d9b75195a6fde9d0a98f991bc32791d
clearinfo <songxian@163.com>
2017-07-23 16:11:48 +0800

    song

--- main/wxwmwreport/index.jsx
+++ main/wxwmwreport/index.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './static/css/index.css';
 import ZmitiUserList  from '../components/zmiti-user-list.jsx';
 
-import {Cascader,message,Select,Modal,Form,Icon,Input,Button, Row, Col,Switch,Radio,InputNumber,Popconfirm,DatePicker,Table ,moment,Tree  } from '../commoncomponent/common.jsx';
+import {Cascader,message,Select,Modal,Form,Icon,Input,Button, Row, Col,Switch,Radio,InputNumber,Popconfirm,DatePicker,Table ,moment,Checkbox  } from '../commoncomponent/common.jsx';
 import 'moment/locale/zh-cn';
 moment.locale('zh-cn');
 const Option = Select.Option;
@@ -15,10 +15,13 @@ import ZmitiUploadDialog from '../components/zmiti-upload-dialog.jsx';
 const FormItem = Form.Item;
 import {ZmitiValidateUser} from '../public/validate-user.jsx';
 const RangePicker = DatePicker.RangePicker;
-const TreeNode = Tree.TreeNode;
 const Search = Input.Search;
-import $ from 'jquery';
+const CheckboxGroup = Checkbox.Group;
 
+function onChange(checkedValues) {
+  console.log('checked = ', checkedValues);
+}
+import $ from 'jquery';
 
 class ZmitwxWmwReportApp extends Component {
 
@@ -48,6 +51,7 @@ class ZmitwxWmwReportApp extends Component {
         };
 
 
+
         
         var title = this.props.params.title || '身边文明事儿';
 
@@ -70,91 +74,123 @@ class ZmitwxWmwReportApp extends Component {
                         </Row>                      
                     </div>
                     <div className="zmiti-wmwreport-line"></div>
+                    <div className='zmiti-wmwreport-tool'>
+	                    <span className="zmiti-wmwreport-all">
+	                    	<Checkbox onChange={onChange}>全选</Checkbox>
+	                    </span>
+	                    <span className="zmiti-wmwreport-category">
+	                    	<Select defaultValue="0" style={{ width: 100 }}>
+						      <Option value="0">-全部-</Option>
+						      <Option value="1">好人好事</Option>
+						      <Option value="2">身边文明事</Option>
+						      <Option value="3">文明播报</Option>
+						    </Select>
+	                    </span>
+	                    <div className="zmiti-wmwreport-search">
+	                    	<Search
+							    placeholder="内容文字"
+							    style={{ width: 200 }}
+							    onSearch={value => console.log(value)}
+							  />
+	                    </div>
+	                    <div className='clearfix'></div>
+                    </div>
                     <div className="zmiti-wmwreport-list">
-                    	<ul>
-                    		<li>
+
                     			<table>
                     				<tbody>
 	                    				<tr>
-	                    					<td width='50'>
-	                    						<input type="checkbox"/>
-	                    					</td>
-	                    					<td width='50'>
-	                    						<img src="http://img2.duitang.com/uploads/item/201201/03/20120103124956_KtWQG.thumb.600_0.jpg" className="zmiti-wmwreport-face"/>
-	                    					</td>
 	                    					<td>
-	                    						<div className="zmiti-wmwreport-username">驹萌宝</div>
-
-	                    						<div className='zmiti-wmwreport-del'><Button icon="delete">删除</Button></div>
-	                    						<div className='clearfix'></div>
-	                    						<div className="zmiti-wmwreport-des"><span className="zmiti-wmwreport-type">#好人365#</span>王金清是内蒙古包头黄河岸边一名普通的农民，从17岁时第一次下水救人，他就再也没有停下来，至今已救出180多条生命。每年黄河开河、封河时，他披星带月、风雨无阻地义务巡堤、帮人救人。2013年4月，王金清将黄河边附近的6位村民组织起来，成立了内蒙古首支黄河水上民间救援队——“王三黄河水上救援队”。从此，一群朴实的农民，在黄河岸边竖起了一面生命的安全屏障。<span className='zmiti-wmwreport-open'>展开全文<Icon type="down" /></span></div>
-	                    						<div>
-	                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx1.sinaimg.cn/mw690/7e56f833ly1fgzgzr8b8sj20jg0db77i.jpg'/>
-	                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx3.sinaimg.cn/mw690/7e56f833gy1fh0lfheq9rj20go0b4n03.jpg'/>
-	                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx3.sinaimg.cn/mw690/7e56f833gy1fhfn3jr0n4j20jg0cyn07.jpg'/>
-	                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx4.sinaimg.cn/mw690/7e56f833ly1fh7joa9r84j20jg0ifgoj.jpg'/>
-	                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx1.sinaimg.cn/mw690/7e56f833ly1fh9v7e1kw5j20jg0czq7g.jpg'/>
 	                    						
-	                    						</div>
-	                    						<div className='zmiti-wmwreport-act'>
-	                    							<div className='zmiti-wmwreport-left'>
-		                    							<span className='zmiti-wmwreport-time'>2017年7月6日 08:36</span>
-		                    							<Button icon="video-camera">查看视频</Button>
-	                    							</div>
-	                    							<div className='zmiti-wmwreport-right'>
-	                    								<Button className='zmiti-wmwreport-message' icon="message">查看评论（10001）条</Button>
-	                    							</div>
-	                    							<div className='clearfix'></div>
+	                    						<div  className='zmiti-wmwreport-item'>
+		                    						<div className='zmiti-wmwreport-con'>
+														<div className='zmiti-wmwreport-imgs'>
+				                    						
+				                    							<img src="http://img2.duitang.com/uploads/item/201201/03/20120103124956_KtWQG.thumb.600_0.jpg" className="zmiti-wmwreport-face"/>
+				                    							
+				                    						
+			                    						</div>
+			                    						<div className="zmiti-wmwreport-username">驹萌宝</div>
+
+			                    						<div className='zmiti-wmwreport-del'><Button icon="delete">删除</Button></div>
+			                    						<div className='clearfix'></div>
+			                    						<div className="zmiti-wmwreport-des"><span className="zmiti-wmwreport-type">#好人365#</span>王金清是内蒙古包头黄河岸边一名普通的农民，从17岁时第一次下水救人，他就再也没有停下来，至今已救出180多条生命。每年黄河开河、封河时，他披星带月、风雨无阻地义务巡堤、帮人救人。2013年4月，王金清将黄河边附近的6位村民组织起来，成立了内蒙古首支黄河水上民间救援队——“王三黄河水上救援队”。从此，一群朴实的农民，在黄河岸边竖起了一面生命的安全屏障。<span className='zmiti-wmwreport-open'>展开全文<Icon type="down" /></span></div>
+			                    						<div>
+			                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx1.sinaimg.cn/mw690/7e56f833ly1fgzgzr8b8sj20jg0db77i.jpg'/>
+			                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx3.sinaimg.cn/mw690/7e56f833gy1fh0lfheq9rj20go0b4n03.jpg'/>
+			                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx3.sinaimg.cn/mw690/7e56f833gy1fhfn3jr0n4j20jg0cyn07.jpg'/>
+			                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx4.sinaimg.cn/mw690/7e56f833ly1fh7joa9r84j20jg0ifgoj.jpg'/>
+			                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx1.sinaimg.cn/mw690/7e56f833ly1fh9v7e1kw5j20jg0czq7g.jpg'/>
+			                    						
+			                    						</div>
+		                    						</div>
+		                    						<div className='clearfix'></div>
+		                    						<div className='zmiti-wmwreport-act'>
+		                    							<div className='zmiti-wmwreport-check'>		                    								
+		                    								<Checkbox onChange={onChange}>选择</Checkbox>
+		                    							</div>
+		                    							<div className='zmiti-wmwreport-left'>
+			                    							<span className='zmiti-wmwreport-time'>2017年7月6日 08:36</span>
+			                    							<Button icon="video-camera">查看视频</Button>
+		                    							</div>
+		                    							<div className='zmiti-wmwreport-right'>
+		                    								<Button className='zmiti-wmwreport-message' icon="message">查看评论（10001）条</Button>
+		                    							</div>
+		                    							<div className='clearfix'></div>
+		                    						</div>
 	                    						</div>
 	                    					</td>
 	                    					
 	                    				</tr>
-                    				</tbody>
-
-                    			</table>
-                    		</li>
-                    		<li>
-                    			<table>
-                    				<tbody>
 	                    				<tr>
-	                    					<td width='50'>
-	                    						<input type="checkbox"/>
-	                    					</td>
-	                    					<td width='50'>
-	                    						<img src="http://img2.duitang.com/uploads/item/201201/03/20120103124956_KtWQG.thumb.600_0.jpg" className="zmiti-wmwreport-face"/>
-	                    					</td>
 	                    					<td>
-	                    						<div className="zmiti-wmwreport-username">驹萌宝</div>
-
-	                    						<div className='zmiti-wmwreport-del'><Button icon="delete">删除</Button></div>
-	                    						<div className='clearfix'></div>
-	                    						<div className="zmiti-wmwreport-des"><span className="zmiti-wmwreport-type">#好人365#</span>王金清是内蒙古包头黄河岸边一名普通的农民，从17岁时第一次下水救人，他就再也没有停下来，至今已救出180多条生命。每年黄河开河、封河时，他披星带月、风雨无阻地义务巡堤、帮人救人。2013年4月，王金清将黄河边附近的6位村民组织起来，成立了内蒙古首支黄河水上民间救援队——“王三黄河水上救援队”。从此，一群朴实的农民，在黄河岸边竖起了一面生命的安全屏障。<span className='zmiti-wmwreport-open'>展开全文<Icon type="down" /></span></div>
-	                    						<div>
-	                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx1.sinaimg.cn/mw690/7e56f833ly1fgzgzr8b8sj20jg0db77i.jpg'/>
-	                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx3.sinaimg.cn/mw690/7e56f833gy1fh0lfheq9rj20go0b4n03.jpg'/>
-	                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx3.sinaimg.cn/mw690/7e56f833gy1fhfn3jr0n4j20jg0cyn07.jpg'/>
-	                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx4.sinaimg.cn/mw690/7e56f833ly1fh7joa9r84j20jg0ifgoj.jpg'/>
-	                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx1.sinaimg.cn/mw690/7e56f833ly1fh9v7e1kw5j20jg0czq7g.jpg'/>
 	                    						
-	                    						</div>
-	                    						<div className='zmiti-wmwreport-act'>
-	                    							<div className='zmiti-wmwreport-left'>
-	                    								<span className='zmiti-wmwreport-time'>2017年7月6日 08:36</span>
-	                    								<Button icon="video-camera">查看视频</Button>
-	                    							</div>
-	                    							<div className='zmiti-wmwreport-right'>
-	                    								<Button className='zmiti-wmwreport-adopt' icon="check-circle">通过审核</Button>
-	                    								<Button className='zmiti-wmwreport-refuse' icon="close-circle">拒绝</Button>
-	                    							</div>
+	                    						<div  className='zmiti-wmwreport-item'>
+		                    						<div className='zmiti-wmwreport-con'>
+														<div className='zmiti-wmwreport-imgs'>
+				                    						
+				                    							<img src="http://img2.duitang.com/uploads/item/201201/03/20120103124956_KtWQG.thumb.600_0.jpg" className="zmiti-wmwreport-face"/>
+				                    							
+				                    						
+			                    						</div>
+			                    						<div className="zmiti-wmwreport-username">驹萌宝</div>
+
+			                    						<div className='zmiti-wmwreport-del'><Button icon="delete">删除</Button></div>
+			                    						<div className='clearfix'></div>
+			                    						<div className="zmiti-wmwreport-des"><span className="zmiti-wmwreport-type">#好人365#</span>王金清是内蒙古包头黄河岸边一名普通的农民，从17岁时第一次下水救人，他就再也没有停下来，至今已救出180多条生命。每年黄河开河、封河时，他披星带月、风雨无阻地义务巡堤、帮人救人。2013年4月，王金清将黄河边附近的6位村民组织起来，成立了内蒙古首支黄河水上民间救援队——“王三黄河水上救援队”。从此，一群朴实的农民，在黄河岸边竖起了一面生命的安全屏障。<span className='zmiti-wmwreport-open'>展开全文<Icon type="down" /></span></div>
+			                    						<div>
+			                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx1.sinaimg.cn/mw690/7e56f833ly1fgzgzr8b8sj20jg0db77i.jpg'/>
+			                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx3.sinaimg.cn/mw690/7e56f833gy1fh0lfheq9rj20go0b4n03.jpg'/>
+			                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx3.sinaimg.cn/mw690/7e56f833gy1fhfn3jr0n4j20jg0cyn07.jpg'/>
+			                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx4.sinaimg.cn/mw690/7e56f833ly1fh7joa9r84j20jg0ifgoj.jpg'/>
+			                    							<img className='zmiti-wmwreport-thumbnail' src='http://wx1.sinaimg.cn/mw690/7e56f833ly1fh9v7e1kw5j20jg0czq7g.jpg'/>
+			                    						
+			                    						</div>
+		                    						</div>
+		                    						<div className='clearfix'></div>
+		                    						<div className='zmiti-wmwreport-act'>
+		                    							<div className='zmiti-wmwreport-check'>
+		                    								<Checkbox onChange={onChange}>选择</Checkbox>
+		                    							</div>
+		                    							<div className='zmiti-wmwreport-left'>
+			                    							<span className='zmiti-wmwreport-time'>2017年7月6日 08:36</span>
+			                    							<Button icon="video-camera">查看视频</Button>
+		                    							</div>
+		                    							<div className='zmiti-wmwreport-right'>
+		                    								<Button className='zmiti-wmwreport-adopt' icon="check-circle">通过审核</Button>
+		                    								<Button className='zmiti-wmwreport-refuse' icon="close-circle">拒绝</Button>
+		                    							</div>
+		                    							<div className='clearfix'></div>
+		                    						</div>
 	                    						</div>
 	                    					</td>
 	                    					
 	                    				</tr>
+
                     				</tbody>
 
                     			</table>
-                    		</li>
-                    	</ul>
+
                     </div>
                 </div>
                 
